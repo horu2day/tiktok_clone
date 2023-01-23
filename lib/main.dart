2855ffa86fe5a2f66db3a2d08eb783589652e89d
horu2day <horu2day@gmail.com>
2023-01-23 17:58:40 +0900

    #3.1 Constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -15,7 +17,16 @@ class TikTokApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: const Color(0xffe9435a),
       ),
-      home: Container(),
+      home: Padding(
+        padding: const EdgeInsets.all(Sizes.size14),
+        child: Container(
+          child: Row(children: const [
+            Text('Hello'),
+            Gaps.h20,
+            Text('Hello'),
+          ]),
+        ),
+      ),
     );
   }
 }
