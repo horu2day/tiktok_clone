ee0f6004351cc3d825d71338aa622345847154aa
horu2day <horu2day@gmail.com>
2023-01-24 22:59:31 +0900

    4-2 중간

--- lib/screens/features/authentication/widgets/auth_button.dart
+++ lib/screens/features/authentication/widgets/auth_button.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class AuthButton extends StatelessWidget {
   final String text;
-  const AuthButton({super.key, required this.text});
+  final FaIcon icon;
+  const AuthButton({super.key, required this.text, required this.icon});
 
   @override
   Widget build(BuildContext context) {
