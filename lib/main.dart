a2ca73b377e233cb64fe16894b830e4ecc85eaed
nbright <kms3700@gmail.com>
2023-01-26 08:51:25 +0900

    #4.3 Sign Up Form -1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tiktok_clone/screens/features/authentication/sign_up_screen.dart';
+import 'package:tiktok_clone/screens/features/authentication/widgets/sign_up_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
---
+++ lib/screens/features/authentication/widgets/login_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  void onSignUpTap(BuildContext context) {
+    Navigator.of(context).pop();
+    // Navigator.of(context).push(
+    //   MaterialPageRoute(
+    //     builder: (context) => const SignUpScreen(),
+    //   ),
+    // );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size40),
+          child: Column(
+            children: const [
+              Gaps.v80,
+              Text(
+                "Login To TikTok",
+                style: TextStyle(
+                  fontSize: Sizes.size24,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Gaps.v20,
+              Text(
+                "Manage your account, check notifications, comment on videos, and more",
+                style: TextStyle(
+                  fontSize: Sizes.size16,
+                  color: Colors.black45,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.grey.shade100,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: Sizes.size32),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Don's have an account?"),
+              Gaps.h5,
+              GestureDetector(
+                onTap: () => onSignUpTap(context),
+                child: Text(
+                  'Sign up',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Theme.of(context).primaryColor),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
