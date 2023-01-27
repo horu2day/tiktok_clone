1ea3dff2a027e66baca8dbe14de3c9bba7136186
nbright <kms3700@gmail.com>
2023-01-27 19:54:50 +0900

    UsernameScreen

--- lib/screens/features/authentication/widgets/email_screen.dart
+++ lib/screens/features/authentication/widgets/email_screen.dart
@@ -1,15 +1,1 @@
-import 'package:flutter/material.dart';
 
-class EmailSreen extends StatelessWidget {
-  const EmailSreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Sign Up'),
-      ),
-    );
-  }
-}
