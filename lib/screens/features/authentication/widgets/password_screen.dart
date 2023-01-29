4bdf45698a057fb1c4965fae83e8a2e03699e994
horu2day <horu2day@gmail.com>
2023-01-29 22:37:29 +0900

    #4.7 Password Screen

--- lib/screens/features/authentication/widgets/password_screen.dart
+++ lib/screens/features/authentication/widgets/password_screen.dart
@@ -1,11 +1,125 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/screens/features/authentication/widgets/form_button.dart';
 
-class PasswordScreen extends StatelessWidget {
+class PasswordScreen extends StatefulWidget {
   const PasswordScreen({super.key});
 
   @override
+  State<PasswordScreen> createState() => _PasswordScreenState();
+}
+
+class _PasswordScreenState extends State<PasswordScreen> {
+  final TextEditingController _passwordController = TextEditingController();
+  String _password = '';
+  @override
+  void initState() {
+    super.initState();
+    _passwordController.addListener(() {
+      setState(() {
+        _password = _passwordController.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  String? _ispasswordValid() {
+    if (_password.isEmpty) return null;
+    final regExp = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    if (!regExp.hasMatch(_password)) {
+      return 'password Not valid';
+    }
+
+    return null;
+  }
+
+  void _onScaffoldTap() {
+    FocusScope.of(context).unfocus();
+  }
+
+  void _onSubmit() {
+    if (_password.isEmpty || _ispasswordValid() != null) return;
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const PasswordScreen(),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Sign Up'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size36),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Gaps.v40,
+            const Text(
+              'Password?',
+              style: TextStyle(
+                  fontSize: Sizes.size20, fontWeight: FontWeight.w600),
+            ),
+            Gaps.v16,
+            TextField(
+              controller: _passwordController,
+              autocorrect: false,
+              onEditingComplete: _onSubmit,
+              decoration: InputDecoration(
+                suffix: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.solidCircleXmark,
+                      color: Colors.grey.shade400,
+                      size: Sizes.size20,
+                    ),
+                    Gaps.h20,
+                    FaIcon(
+                      FontAwesomeIcons.eye,
+                      color: Colors.grey.shade500,
+                      size: Sizes.size20,
+                    ),
+                  ],
+                ),
+                hintText: 'Make it strong!',
+                errorText: _ispasswordValid(),
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                  color: Colors.grey.shade400,
+                )),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+              ),
+              cursorColor: Theme.of(context).primaryColor,
+            ),
+            Gaps.v28,
+            GestureDetector(
+              onTap: _onSubmit,
+              child: FormButton(
+                disabled: _password.isEmpty || _ispasswordValid() != null,
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/features/authentication/widgets/username_screen.dart
+++ lib/screens/features/authentication/widgets/username_screen.dart
@@ -39,49 +39,57 @@ class _UsernameScreenState extends State<UsernameScreen> {
     );
   }
 
+  void _onScaffoldTap() {
+    FocusScope.of(context).unfocus();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Sign Up'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: Sizes.size36),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Gaps.v40,
-          const Text(
-            'Create username',
-            style:
-                TextStyle(fontSize: Sizes.size20, fontWeight: FontWeight.w600),
-          ),
-          Gaps.v8,
-          const Text(
-            'You can always change this later',
-            style: TextStyle(fontSize: Sizes.size16, color: Colors.black54),
-          ),
-          TextField(
-            controller: _usernameController,
-            decoration: InputDecoration(
-              hintText: 'Username',
-              enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                color: Colors.grey.shade400,
-              )),
-              focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Sign Up'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size36),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Gaps.v40,
+            const Text(
+              'Create username',
+              style: TextStyle(
+                  fontSize: Sizes.size20, fontWeight: FontWeight.w600),
+            ),
+            Gaps.v8,
+            const Text(
+              'You can always change this later',
+              style: TextStyle(fontSize: Sizes.size16, color: Colors.black54),
+            ),
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                hintText: 'Username',
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
                   color: Colors.grey.shade400,
+                )),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
                 ),
               ),
+              cursorColor: Theme.of(context).primaryColor,
             ),
-            cursorColor: Theme.of(context).primaryColor,
-          ),
-          Gaps.v28,
-          GestureDetector(
-            onTap: onNextTap,
-            child: FormButton(disabled: _username.isEmpty),
-          ),
-        ]),
+            Gaps.v28,
+            GestureDetector(
+              onTap: onNextTap,
+              child: FormButton(disabled: _username.isEmpty),
+            ),
+          ]),
+        ),
       ),
     );
   }
