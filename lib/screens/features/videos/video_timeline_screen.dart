d013ed79de355bb417c24e4035e388bc2d9b05bb
nbright <kms3700@gmail.com>
2023-02-09 20:35:28 +0900

    #7.3 Video Player

--- lib/screens/features/videos/video_timeline_screen.dart
+++ lib/screens/features/videos/video_timeline_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/screens/features/videos/widgets/video_post.dart';
 
 class VideoTimelineScreen extends StatefulWidget {
   const VideoTimelineScreen({super.key});
@@ -42,15 +43,7 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
       scrollDirection: Axis.vertical,
       onPageChanged: _onPageChanged,
       itemCount: _itemCount,
-      itemBuilder: (context, index) => Container(
-        color: colors[index],
-        child: Center(
-          child: Text(
-            "Screen $index",
-            style: const TextStyle(fontSize: 68),
-          ),
-        ),
-      ),
+      itemBuilder: (context, index) => const VideoPost(),
       // children: [
       //   Container(
       //     color: Colors.blue,
--- lib/screens/features/videos/widgets/video_post.dart
+++ lib/screens/features/videos/widgets/video_post.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoPost extends StatefulWidget {
   const VideoPost({super.key});
@@ -8,8 +9,33 @@ class VideoPost extends StatefulWidget {
 }
 
 class _VideoPostState extends State<VideoPost> {
+  final VideoPlayerController _videoPlayerController =
+      VideoPlayerController.asset("assets/videos/video.mp4");
+
+  void _initVideoPlayer() async {
+    await _videoPlayerController.initialize();
+    _videoPlayerController.play();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initVideoPlayer();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Stack();
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: _videoPlayerController.value.isInitialized
+              ? VideoPlayer(_videoPlayerController)
+              : Container(
+                  color: Colors.black,
+                ),
+        ),
+      ],
+    );
   }
 }
