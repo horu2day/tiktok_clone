928e3ad16137479fb8962dd34ad2545c70c7b832
horu2day <horu2day@gmail.com>
2023-03-05 20:02:36 +0900

    #9.4 Grid Item

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 final tabs = [
@@ -47,20 +49,72 @@ class DiscoverScreen extends StatelessWidget {
         body: TabBarView(
           children: [
             GridView.builder(
-              itemCount: 20,
-              padding: const EdgeInsets.symmetric(
-                horizontal: Sizes.size6,
-              ),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: Sizes.size10,
-                  mainAxisSpacing: Sizes.size10,
-                  childAspectRatio: 9 / 16),
-              itemBuilder: ((context, index) => Container(
-                    color: Colors.teal,
-                    child: Center(child: Text("$index")),
-                  )),
-            ),
+                itemCount: 20,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: Sizes.size6,
+                ),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: Sizes.size10,
+                    mainAxisSpacing: Sizes.size10,
+                    childAspectRatio: 9 / 21),
+                itemBuilder: (context, index) => Column(
+                      children: [
+                        AspectRatio(
+                          aspectRatio: 9 / 16,
+                          child: FadeInImage.assetNetwork(
+                            fit: BoxFit.cover,
+                            placeholder: "assets/images/placeholder.jpg",
+                            image:
+                                "https://p4.wallpaperbetter.com/wallpaper/63/921/193/anime-anime-girls-neon-genesis-evangelion-rebuild-of-evangelion-super-robot-taisen-hd-wallpaper-preview.jpg",
+                          ),
+                        ),
+                        Gaps.v10,
+                        const Text(
+                          "This is a very long caption for my tiktok that im upload just now currently.",
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                          style: TextStyle(
+                            fontSize: Sizes.size16 + Sizes.size2,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Gaps.v8,
+                        DefaultTextStyle(
+                          style: TextStyle(
+                            color: Colors.grey.shade600,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          child: Row(
+                            children: [
+                              const CircleAvatar(
+                                radius: 12,
+                                backgroundImage: NetworkImage(
+                                    "https://avatars.githubusercontent.com/u/594733?s=400&u=51d0a83f972e0f874318c581a91cf0247a927773&v=4"),
+                              ),
+                              Gaps.h4,
+                              const Expanded(
+                                child: Text(
+                                  "My avartar is going to be very long",
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                              Gaps.h4,
+                              FaIcon(
+                                FontAwesomeIcons.heart,
+                                size: Sizes.size16,
+                                color: Colors.grey.shade600,
+                              ),
+                              Gaps.h2,
+                              const Text(
+                                "2.5M",
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    )),
             for (var tab in tabs.skip(1))
               Center(
                 child: Text(
