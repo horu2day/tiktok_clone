61619160ce33c05d40f1f6d5364a4a2c2015faf9
horu2day <horu2day@gmail.com>
2023-03-18 19:45:31 +0900

    #11.4 Chat Detail

---
+++ lib/features/inbox/chat_detail_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ChatDetailScreen extends StatefulWidget {
+  const ChatDetailScreen({super.key});
+
+  @override
+  State<ChatDetailScreen> createState() => _ChatDetailScreenState();
+}
+
+class _ChatDetailScreenState extends State<ChatDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/inbox/chat_detail_screen.dart';
 
 class ChatsScreen extends StatefulWidget {
   const ChatsScreen({super.key});
@@ -10,6 +11,75 @@ class ChatsScreen extends StatefulWidget {
 }
 
 class _ChatsScreenState extends State<ChatsScreen> {
+  final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+
+  final List<int> _items = [];
+
+  final Duration _duration = const Duration(milliseconds: 300);
+
+  void _addItem() {
+    if (_key.currentState != null) {
+      _key.currentState!.insertItem(_items.length, duration: _duration);
+      _items.add(_items.length);
+    }
+  }
+
+  void _deleteItem(int index) {
+    if (_key.currentContext != null) {
+      _key.currentState!.removeItem(
+        index,
+        (context, animation) => SizeTransition(
+          sizeFactor: animation,
+          child: Container(color: Colors.red, child: _makeTile(index)),
+        ),
+        duration: _duration,
+      );
+      _items.removeAt(index);
+    }
+  }
+
+  void _onChatTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const ChatDetailScreen(),
+      ),
+    );
+  }
+
+  Widget _makeTile(index) {
+    return ListTile(
+      onLongPress: () => _deleteItem(index),
+      onTap: _onChatTap,
+      leading: const CircleAvatar(
+        radius: 30,
+        backgroundImage: NetworkImage(
+          "https://avatars.githubusercontent.com/u/594733?s=400&u=51d0a83f972e0f874318c581a91cf0247a927773&v=4",
+        ),
+        child: Text('nico'),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            "Lynn ($index)",
+            style: const TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Text(
+            "2:16 PM",
+            style: TextStyle(
+              color: Colors.grey.shade500,
+              fontSize: Sizes.size14,
+            ),
+          ),
+        ],
+      ),
+      subtitle: const Text("Don't forget to make video"),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,46 +89,27 @@ class _ChatsScreenState extends State<ChatsScreen> {
         title: const Text('Direct message'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _addItem,
             icon: const FaIcon(FontAwesomeIcons.plus),
           ),
         ],
       ),
-      body: ListView(
-          padding: const EdgeInsets.symmetric(
-            vertical: Sizes.size10,
-          ),
-          children: [
-            ListTile(
-              leading: const CircleAvatar(
-                radius: 30,
-                backgroundImage: NetworkImage(
-                  "https://avatars.githubusercontent.com/u/594733?s=400&u=51d0a83f972e0f874318c581a91cf0247a927773&v=4",
-                ),
-                child: Text('nico'),
-              ),
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  const Text(
-                    "Lynn",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  Text(
-                    "2:16 PM",
-                    style: TextStyle(
-                      color: Colors.grey.shade500,
-                      fontSize: Sizes.size14,
-                    ),
-                  ),
-                ],
-              ),
-              subtitle: const Text("Don't forget to make video"),
+      body: AnimatedList(
+        key: _key,
+        padding: const EdgeInsets.symmetric(
+          vertical: Sizes.size10,
+        ),
+        itemBuilder: (context, index, animation) {
+          return FadeTransition(
+            key: UniqueKey(),
+            opacity: animation,
+            child: SizeTransition(
+              sizeFactor: animation,
+              child: _makeTile(index),
             ),
-          ]),
+          );
+        },
+      ),
     );
   }
 }
