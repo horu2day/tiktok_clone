fd9d9c87489254b35fb8017c72ac45cffcaf3635
nbright <kms3700@gmail.com>
2023-03-21 14:47:07 +0900

    #11.4 Chat Detail

--- lib/features/inbox/chat_detail_screen.dart
+++ lib/features/inbox/chat_detail_screen.dart
@@ -75,23 +75,66 @@ class _ChatDetailScreenState extends State<ChatDetailScreen> {
       body: Stack(
         children: [
           ListView.separated(
+              padding: const EdgeInsets.symmetric(
+                vertical: Sizes.size20,
+                horizontal: Sizes.size14,
+              ),
               itemBuilder: (context, index) {
                 final isMine = index % 2 == 0;
-                return Container(
-                  decoration: BoxDecoration(
-                    color:
-                        isMine ? Colors.blue : Theme.of(context).primaryColor,
-                  ),
-                  child: const Text(
-                    "This is a message",
-                    style: TextStyle(
-                      color: Colors.white,
+                return Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment:
+                      isMine ? MainAxisAlignment.end : MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(Sizes.size14),
+                      decoration: BoxDecoration(
+                        color: isMine
+                            ? Colors.blue
+                            : Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.only(
+                          topLeft: const Radius.circular(
+                            Sizes.size20,
+                          ),
+                          topRight: const Radius.circular(
+                            Sizes.size20,
+                          ),
+                          bottomLeft: Radius.circular(
+                            isMine ? Sizes.size20 : Sizes.size5,
+                          ),
+                          bottomRight: Radius.circular(
+                            !isMine ? Sizes.size20 : Sizes.size5,
+                          ),
+                        ),
+                      ),
+                      child: const Text(
+                        "This is a message",
+                        style: TextStyle(
+                            color: Colors.white, fontSize: Sizes.size16),
+                      ),
                     ),
-                  ),
+                  ],
                 );
               },
               separatorBuilder: (context, index) => Gaps.v10,
               itemCount: 10),
+          Positioned(
+            bottom: 0,
+            width: MediaQuery.of(context).size.width,
+            child: BottomAppBar(
+              child: Row(
+                children: [
+                  const Expanded(child: TextField()),
+                  Gaps.h20,
+                  Container(
+                    child: const FaIcon(
+                      FontAwesomeIcons.paperPlane,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
