0bba6a8ea498884f1ba4d84c40457fbabfb0ab57
nbright <kms3700@gmail.com>
2023-03-23 08:39:22 +0900

    #12.1 CustomScrollView - 스크롤이 작동안함.

---
+++ lib/features/users/user_profile_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class UserProfileScreen extends StatefulWidget {
+  const UserProfileScreen({super.key});
+
+  @override
+  State<UserProfileScreen> createState() => _UserProfileScreenState();
+}
+
+class _UserProfileScreenState extends State<UserProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      physics: const BouncingScrollPhysics(),
+      slivers: [
+        SliverAppBar(
+          floating: true,
+          stretch: true,
+          pinned: true,
+          backgroundColor: Colors.teal,
+          collapsedHeight: 80,
+          expandedHeight: 200,
+          flexibleSpace: FlexibleSpaceBar(
+            stretchModes: const [
+              StretchMode.blurBackground,
+              StretchMode.zoomBackground,
+            ],
+            background: Image.asset(
+              "assets/images/placeholder.jpg",
+              fit: BoxFit.cover,
+            ),
+            title: const Text("Hello!"),
+          ),
+        )
+      ],
+    );
+  }
+}
