c39b444eb6de88a45b792eb155a87eb9c5e51810
horu2day <horu2day@gmail.com>
2023-03-30 06:22:16 +0900

    #12.7 PersistentTabBar -1

---
+++ lib/features/users/widgets/persistent_tabbar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class PersistentTabbar extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement maxExtent
+  double get maxExtent => throw UnimplementedError();
+
+  @override
+  // TODO: implement minExtent
+  double get minExtent => throw UnimplementedError();
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    // TODO: implement shouldRebuild
+    throw UnimplementedError();
+  }
+}
