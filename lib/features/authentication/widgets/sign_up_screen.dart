03d90fdfaf760371f662704459ba5fd548009c3e
horu2day <horu2day@gmail.com>
2023-04-25 00:31:07 +0900

    googlefont

--- lib/features/authentication/widgets/sign_up_screen.dart
+++ lib/features/authentication/widgets/sign_up_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
@@ -37,7 +38,12 @@ class SignUpScreen extends StatelessWidget {
               Gaps.v80,
               Text(
                 "Sign up for TikTok",
-                style: Theme.of(context).textTheme.headlineSmall,
+                style: GoogleFonts.abrilFatface(
+                  textStyle: const TextStyle(
+                    fontSize: Sizes.size24,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
               ),
               Gaps.v20,
               const Opacity(
--- lib/main.dart
+++ lib/main.dart
@@ -30,34 +30,7 @@ class TikTokApp extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: ThemeData(
         brightness: Brightness.light,
-        textTheme: TextTheme(
-          displayLarge: GoogleFonts.openSans(
-              fontSize: 96, fontWeight: FontWeight.w300, letterSpacing: -1.5),
-          displayMedium: GoogleFonts.openSans(
-              fontSize: 60, fontWeight: FontWeight.w300, letterSpacing: -0.5),
-          displaySmall:
-              GoogleFonts.openSans(fontSize: 48, fontWeight: FontWeight.w400),
-          headlineMedium: GoogleFonts.openSans(
-              fontSize: 34, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-          headlineSmall:
-              GoogleFonts.openSans(fontSize: 24, fontWeight: FontWeight.w400),
-          titleLarge: GoogleFonts.openSans(
-              fontSize: 20, fontWeight: FontWeight.w500, letterSpacing: 0.15),
-          titleMedium: GoogleFonts.openSans(
-              fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.15),
-          titleSmall: GoogleFonts.openSans(
-              fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
-          bodyLarge: GoogleFonts.roboto(
-              fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
-          bodyMedium: GoogleFonts.roboto(
-              fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-          labelLarge: GoogleFonts.roboto(
-              fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
-          bodySmall: GoogleFonts.roboto(
-              fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
-          labelSmall: GoogleFonts.roboto(
-              fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
-        ),
+        textTheme: GoogleFonts.itimTextTheme(),
         scaffoldBackgroundColor: Colors.white,
         primaryColor: const Color(0xffe9435a),
         textSelectionTheme: const TextSelectionThemeData(
@@ -81,6 +54,9 @@ class TikTokApp extends StatelessWidget {
       ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
+        textTheme: GoogleFonts.itimTextTheme(
+          ThemeData(brightness: Brightness.dark).textTheme,
+        ),
         scaffoldBackgroundColor: Colors.black,
         primaryColor: const Color(0xffe9435a),
         bottomAppBarTheme: BottomAppBarTheme(
@@ -118,3 +94,33 @@ class TikTokApp extends StatelessWidget {
 //     );
 //   }
 // }
+
+// 텍스트 Theme
+// textTheme: TextTheme(
+//           displayLarge: GoogleFonts.openSans(
+//               fontSize: 96, fontWeight: FontWeight.w300, letterSpacing: -1.5),
+//           displayMedium: GoogleFonts.openSans(
+//               fontSize: 60, fontWeight: FontWeight.w300, letterSpacing: -0.5),
+//           displaySmall:
+//               GoogleFonts.openSans(fontSize: 48, fontWeight: FontWeight.w400),
+//           headlineMedium: GoogleFonts.openSans(
+//               fontSize: 34, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+//           headlineSmall:
+//               GoogleFonts.openSans(fontSize: 24, fontWeight: FontWeight.w400),
+//           titleLarge: GoogleFonts.openSans(
+//               fontSize: 20, fontWeight: FontWeight.w500, letterSpacing: 0.15),
+//           titleMedium: GoogleFonts.openSans(
+//               fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.15),
+//           titleSmall: GoogleFonts.openSans(
+//               fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
+//           bodyLarge: GoogleFonts.roboto(
+//               fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
+//           bodyMedium: GoogleFonts.roboto(
+//               fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+//           labelLarge: GoogleFonts.roboto(
+//               fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
+//           bodySmall: GoogleFonts.roboto(
+//               fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
+//           labelSmall: GoogleFonts.roboto(
+//               fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
+//         ),
