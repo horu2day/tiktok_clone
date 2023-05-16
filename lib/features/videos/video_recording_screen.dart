afbc19aa4ef5e916e942469fb339def174f446e5
nbright <kms3700@gmail.com>
2023-05-16 20:40:47 +0900

    #19.2 CameraController

--- lib/features/videos/video_recording_screen.dart
+++ lib/features/videos/video_recording_screen.dart
@@ -1,4 +1,9 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import '../../constants/gaps.dart';
+import '../../constants/sizes.dart';
 
 class VideoRecordingScreen extends StatefulWidget {
   const VideoRecordingScreen({super.key});
@@ -8,13 +13,71 @@ class VideoRecordingScreen extends StatefulWidget {
 }
 
 class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
+  bool _hasPermission = false;
+  bool _deniedPermission = false;
+
+  late final CameraController _cameraController;
+
+  Future<void> initCamera() async {
+    final cameras = await availableCameras();
+    if (cameras.isEmpty) {
+      return;
+    }
+    _cameraController = CameraController(
+      cameras[0],
+      ResolutionPreset.ultraHigh,
+    );
+
+    await _cameraController.initialize();
+  }
+
+  Future<void> initPermission() async {
+    final cameraPermission = await Permission.camera.request();
+    final micPermission = await Permission.microphone.request();
+
+    final cameraDenied =
+        cameraPermission.isDenied || cameraPermission.isPermanentlyDenied;
+    final micDenied =
+        micPermission.isDenied || micPermission.isPermanentlyDenied;
+    if (!cameraDenied && !micDenied) {
+      _hasPermission = true;
+      await initCamera();
+      setState(() {});
+    } else {
+      _deniedPermission = true;
+      //이건 아니지 권한을 허용해 달라구.
+    }
+  }
+
+  @override
+  void initState() {
+    initPermission();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text("hello!"),
-      ),
+      backgroundColor: Colors.black,
+      // 권한을 가지고 있지 않거나 카메라가 초기화 되지 않았으면
+      body: !_hasPermission || !_cameraController.value.isInitialized
+          ? SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Initializing...",
+                    style:
+                        TextStyle(color: Colors.white, fontSize: Sizes.size20),
+                  ),
+                  Gaps.v20,
+                  CircularProgressIndicator.adaptive(),
+                ],
+              ),
+            )
+          : CameraPreview(_cameraController),
     );
   }
 }
