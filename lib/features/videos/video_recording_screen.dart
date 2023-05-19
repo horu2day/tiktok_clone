ace5787df635955bb0bf5331f9ca62c92553cab3
nbright <kms3700@gmail.com>
2023-05-19 18:00:40 +0900

    a

--- lib/features/videos/video_recording_screen.dart
+++ lib/features/videos/video_recording_screen.dart
@@ -16,6 +16,8 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
   bool _hasPermission = false;
   bool _deniedPermission = false;
   bool _isSelfieMode = false;
+
+  late FlashMode _flashMode;
   late CameraController _cameraController;
 
   Future<void> initCamera() async {
@@ -29,6 +31,8 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
     );
 
     await _cameraController.initialize();
+
+    _flashMode = _cameraController.value.flashMode;
   }
 
   Future<void> initPermission() async {
@@ -58,6 +62,12 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
   Future<void> _toggleSelfieMode() async {
     _isSelfieMode = !_isSelfieMode;
     await initCamera();
+    setState(() {});
+  }
+
+  Future<void> _setFlashMode(FlashMode newFlashMode) async {
+    await _cameraController.setFlashMode(newFlashMode);
+    _flashMode = newFlashMode;
     setState(() {});
   }
 
@@ -104,14 +114,84 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
                   CameraPreview(_cameraController),
                   Positioned(
                       top: Sizes.size20,
-                      left: Sizes.size20,
-                      child: IconButton(
-                        color: Colors.white,
-                        onPressed: _toggleSelfieMode,
-                        icon: const Icon(Icons.cameraswitch),
+                      right: Sizes.size20,
+                      child: Column(
+                        children: [
+                          IconButton(
+                            color: Colors.white,
+                            onPressed: _toggleSelfieMode,
+                            icon: const Icon(
+                              Icons.cameraswitch,
+                            ),
+                          ),
+                          Gaps.v10,
+                          IconButton(
+                            color: _flashMode == FlashMode.off
+                                ? Colors.amber.shade200
+                                : Colors.white,
+                            onPressed: () {}, //=> _setFlashMode(FlashMode.off),
+                            icon: const Icon(
+                              Icons.flash_off_rounded,
+                            ),
+                          ),
+                          Gaps.v10,
+                          IconButton(
+                            color: _flashMode == FlashMode.always
+                                ? Colors.amber.shade200
+                                : Colors.white,
+                            onPressed:
+                                () {}, //=> _setFlashMode(FlashMode.always),
+                            icon: const Icon(
+                              Icons.flash_on_rounded,
+                            ),
+                          ),
+                          Gaps.v10,
+                          IconButton(
+                            color: _flashMode == FlashMode.auto
+                                ? Colors.amber.shade200
+                                : Colors.white,
+                            onPressed:
+                                () {}, //=> _setFlashMode(FlashMode.auto),
+                            icon: const Icon(
+                              Icons.flash_auto_rounded,
+                            ),
+                          ),
+                          Gaps.v10,
+                          FlashModeButton(
+                            flashMode: _flashMode,
+                            flashType: FlashMode.torch,
+                            setFlashMode: _setFlashMode,
+                            iconData: Icons.flashlight_on_rounded,
+                          ),
+                        ],
                       )),
                 ],
               ),
+      ),
+    );
+  }
+}
+
+class FlashModeButton extends StatelessWidget {
+  const FlashModeButton({
+    super.key,
+    required this.flashMode,
+    required this.flashType,
+    required this.setFlashMode,
+    required this.iconData,
+  });
+
+  final FlashMode flashMode;
+  final FlashMode flashType;
+  final IconData iconData;
+  final Future<void> Function(FlashMode newFlashMode) setFlashMode;
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      color: flashMode == flashType ? Colors.amber.shade200 : Colors.white,
+      onPressed: () => setFlashMode(flashType),
+      icon: Icon(
+        iconData,
       ),
     );
   }
