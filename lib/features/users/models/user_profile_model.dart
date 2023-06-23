e230835aacee5532a6ccc5c223c18ec714f58549
nbright <kms3700@gmail.com>
2023-06-23 17:58:21 +0900

    aaa

---
+++ lib/features/users/models/user_profile_model.dart
@@ -0,0 +1,16 @@
+class UserProfileModel {
+  final String uid;
+  final String email;
+  final String name;
+
+  final String bio;
+  final String link;
+
+  UserProfileModel({
+    required this.uid,
+    required this.email,
+    required this.name,
+    required this.bio,
+    required this.link,
+  });
+}
---
+++ lib/features/users/view_models/user_view_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tiktok_clone/features/users/models/user_profile_model.dart';
+
+class UserViewModel extends AsyncNotifier<UserProfileModel>{
+  
+}
+
+final usersProvider 
