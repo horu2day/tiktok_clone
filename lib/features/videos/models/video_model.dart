40dbb0ca314cf6af30424d8c5b9e34e4e3d31cd1
김민성 <kms3700@gmail.com>
2023-07-12 09:57:04 +0900

    #27.1 VideoPost-1

--- lib/features/videos/models/video_model.dart
+++ lib/features/videos/models/video_model.dart
@@ -19,6 +19,17 @@ class VideoModel {
       required this.comments,
       required this.createdAt});
 
+  VideoModel.fromJson(Map<String, dynamic> json)
+      : title = json["title"],
+        description = json["description"],
+        fileUrl = json["fileUrl"],
+        thumbnailUrl = json["thumbnailUrl"],
+        creatorUid = json["creatorUid"],
+        creator = json["creator"],
+        likes = json["likes"],
+        comments = json["comments"],
+        createdAt = json["createdAt"];
+
   Map<String, dynamic> toJson() {
     return {
       "title": title,
--- lib/features/videos/view_models/timeline_view_model.dart
+++ lib/features/videos/view_models/timeline_view_model.dart
@@ -7,7 +7,7 @@ import '../models/video_model.dart';
 
 class TimelineViewModel extends AsyncNotifier<List<VideoModel>> {
   late final VideosRepository _repository;
-  final List<VideoModel> _list = [];
+  List<VideoModel> _list = [];
 
   // 임시로 만들었던것.
   // void uploadVideo() async {
@@ -22,7 +22,14 @@ class TimelineViewModel extends AsyncNotifier<List<VideoModel>> {
   FutureOr<List<VideoModel>> build() async {
     _repository = ref.read(videoRepo);
     final result = await _repository.fetchVideo();
-    result.docs.map((e) => null);
+    final newList = result.docs.map(
+      (doc) => VideoModel.fromJson(
+        doc.data(),
+      ),
+    );
+    print(newList);
+    //
+    _list = newList.toList();
     //await Future.delayed(const Duration(seconds: 5)); // 나중에 Api에서 온다.
     //에러 발생을 테스트
     //throw Exception("omg can't fetch");
